cecde12572a93a24a74a9ce323e7c2ad984beb3d
michal.nowak <michal.nowak@displate.com>
2024-01-19 21:02:12 +0100

    remove package

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: `~plugins/vimeo-player` }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -75,8 +74,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    vendor: [
-      'vue-vimeo-player'
-    ]
   }
 }
--- plugins/vimeo-player.js
+++
@@ -1,5 +0,0 @@
-// plugins/vimeo-player.js
-import Vue from 'vue'
-import vueVimeoPlayer from 'vue-vimeo-player'
-
-Vue.use(vueVimeoPlayer)
